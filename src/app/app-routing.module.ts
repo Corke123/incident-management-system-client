560f192af9b41d949ed9ea919bfe5c993d63f87f
Bojan Coric <bojancoric996@gmail.com>
2023-06-18 21:33:46 +0200

    Add basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IncidentsMapComponent } from './incidents/incidents-map/incidents-map.component';
+import { CreateIncidentComponent } from './incidents/create-incident/create-incident.component';
+import { ProcessIncidentComponent } from './incidents/process-incident/process-incident.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'incidents', component: IncidentsMapComponent
+  },
+  {
+    path: 'new-incident', component: CreateIncidentComponent
+  },
+  {
+    path: 'process-incident', component: ProcessIncidentComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
